b764051135e17e98645dc0257c6f8832c50fce94
Thomas P Braun <tbraun96@gmail.com>
2021-02-07 12:23:08 -0500

    Added keep-alive timeout variability, useful for android/ios

--- lusna_ffi/src/lib.rs
+++ lusna_ffi/src/lib.rs
@@ -1,7 +1,7 @@
 use android_logger::{Config, FilterBuilder};
 use jni::JNIEnv;
 use jni::objects::{JClass, JObject, JValue};
-use jni::sys::{jbyteArray, jstring};
+use jni::sys::{jbyteArray, jstring, jint};
 use log::Level;
 
 use hyxewave::ffi::KernelResponse;
@@ -17,10 +17,10 @@ pub static FFI_OBJECT: Mutex<Option<FFIObject>> = parking_lot::const_mutex(None)
 
 
 #[no_mangle]
-pub extern "system" fn Java_com_lusna_svc_LusnaService_start_1rust_1subsystem(env: JNIEnv<'static>, clazz: JClass<'static>, home_dir: jbyteArray) -> jstring {
+pub extern "system" fn Java_com_satori_svc_VerisendService_start_1rust_1subsystem(env: JNIEnv<'static>, clazz: JClass<'static>, home_dir: jbyteArray, kthreads: jint) -> jstring {
     let output = format!("Powered by Rust 1.51 nightly ({} {})", std::env::consts::ARCH, std::env::consts::OS);
     let home_dir = env.convert_byte_array(home_dir).unwrap();
-    start_lusnanet(&env, &clazz, home_dir);
+    start_lusnanet(&env, &clazz, home_dir, kthreads as usize);
     // send_to_java(&[1, 2, 3]);
     log::info!("Successfully executed Lusna's rust kernel");
 
@@ -32,7 +32,7 @@ pub extern "system" fn Java_com_lusna_svc_LusnaService_start_1rust_1subsystem(en
 /// This function executes a command against the kernel, then takes the result, wraps it into a vector, and converts the vec into
 /// a jbyteArray. From there, it gets sent back to the FFI
 #[no_mangle]
-pub extern "system" fn Java_com_lusna_svc_LusnaService_send_1data(env: JNIEnv<'static>, _: JClass<'static>, array: jbyteArray) -> jbyteArray {
+pub extern "system" fn Java_com_satori_svc_VerisendService_send_1data(env: JNIEnv<'static>, _: JClass<'static>, array: jbyteArray) -> jbyteArray {
     let data = env.convert_byte_array(array).unwrap();
     let kernel_response = KernelResponse::from(hyxewave::ffi::command_handler::on_ffi_bytes_received(data));
     env.byte_array_from_slice(serialize(kernel_response).as_slice()).unwrap()
@@ -70,14 +70,17 @@ fn get_rust_to_native_fn() -> Box<dyn Fn(Result<Option<KernelResponse>, ConsoleE
 }
 
 /// When the java module called get_rust_version on init, this function gets called.
-#[allow(unused_results)]
-fn start_lusnanet(env: &JNIEnv<'static>, clazz: &JClass<'static>, home_dir: Vec<u8>) {
-    FFI_OBJECT.lock().replace(FFIObject::new(env, clazz));
-
+fn start_lusnanet(env: &JNIEnv<'static>, clazz: &JClass<'static>, home_dir: Vec<u8>, kthreads: usize) {
     // start android logger
     start_logger();
+
+    if let Some(_) = FFI_OBJECT.lock().replace(FFIObject::new(env, clazz)) {
+        log::warn!("FFI Object replaced. Instance already running. Will not run again");
+        return;
+    }
+
     let home_dir = String::from_utf8(home_dir).unwrap();
-    let args = format!("--bind 0.0.0.0 --home {}", home_dir);
+    let args = format!("--public --home {} --kthreads {}", home_dir, kthreads);
     log::info!("Will execute the Lusna Kernel with: {}", &args);
 
     // spawn a new thread to not block the FFI call
