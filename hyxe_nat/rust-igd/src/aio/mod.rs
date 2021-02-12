1bdb6b98098700e727dd5aa1a00a6a33c5401cf4
Thomas P Braun <tbraun96@gmail.com>
2021-02-12 14:23:17 -0500

    Fixed concurrency bugs in multi-threaded mode by using immutable borrows of session in PeerCmdPacket instead of mutable ones. This thus allows two packets to come-in at the same time, each targetting each other's implicated CID's in a parallel context

--- hyxe_nat/rust-igd/src/aio/mod.rs
+++
@@ -1,8 +0,0 @@
-//! This module implements the same features as the main crate, but using async io.
-
-mod gateway;
-mod search;
-mod soap;
-
-pub use self::gateway::Gateway;
-pub use self::search::search_gateway;
