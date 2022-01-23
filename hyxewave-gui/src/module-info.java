78f31fdaa4613e937f988ead3a33ddfc17609838
Thomas Braun <thomas.braun@onscale.com>
2022-01-23 10:35:32 -0500

    Cleaning up more

--- hyxewave-gui/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module hyxewave.gui {
-    requires java.datatransfer;
-    requires java.desktop;
-    requires java.logging;
-    requires AbsoluteLayout;
-    requires swing.layout;
-    requires swingx.all;
-    requires vertx.core;
-}
