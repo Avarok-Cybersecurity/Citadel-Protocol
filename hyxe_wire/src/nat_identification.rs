f5221f12afd4f8b1019a819802fca5b8e26af7bd
Thomas Braun <braunth@oregonstate.edu>
2022-03-14 16:33:41 -0400

    Fixed bug in hole puncher

--- hyxe_wire/src/nat_identification.rs
+++ hyxe_wire/src/nat_identification.rs
@@ -197,6 +197,13 @@ async fn get_nat_type() -> Result<NatType, anyhow::Error> {
                     return Ok(NatType::EDMRandomIp(vec![addr_ext.ip(), addr2_ext.ip(), addr3_ext.ip()], None));
                 }
 
+                // check to see if ext_port == int_port
+                if addr_ext.port() == addr_int.port() && addr2_ext.port() == addr2_int.port() && addr3_ext.port() == addr3_int.port() {
+                    // in this case, the IP changes, however, the port stays the same. The NAT maps as such:
+                    // ip0:port -> ip1:port
+                    return Ok(NatType::EDM(addr_ext.ip(), None, 0))
+                }
+
                 let deltas = &mut [addr_ext.port(), addr2_ext.port(), addr3_ext.port()];
                 deltas.sort();
 
@@ -207,7 +214,7 @@ async fn get_nat_type() -> Result<NatType, anyhow::Error> {
                 let delta0 = i32::abs(deltas[0] as i32 - deltas[1] as i32);
                 let delta1 = i32::abs(deltas[1] as i32 - deltas[2] as i32);
                 //let delta2 = i32::abs(addr3_ext.port() as i32 - addr3_int.port() as i32);
-                log::info!("Delta0: {} | Delta1: {}", delta0, delta1);
+                log::info!("[external] Delta0: {} | Delta1: {}", delta0, delta1);
 
                 return if delta0 == delta1 {
                     // This means the ports are predictable. Use TCP simultaneous connect on expected ports based on delta. It is expected this data be sent to the peer. The peer will then connect to the socket ip:(LOCAL_BIND_PORT+delta)
