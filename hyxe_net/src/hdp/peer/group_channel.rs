0b2536d6594b835d1274c0455841536d93f07679
Thomas Braun <braunth@oregonstate.edu>
2022-05-13 17:09:12 -0400

    changes

--- hyxe_net/src/hdp/peer/group_channel.rs
+++ hyxe_net/src/hdp/peer/group_channel.rs
@@ -148,7 +148,7 @@ impl Stream for GroupChannelRecvHalf {
 impl Drop for GroupChannelRecvHalf {
     fn drop(&mut self) {
         if let Err(err) = inner_mut_state!(self.state_container).process_outbound_broadcast_command(self.ticket, &GroupBroadcast::LeaveRoom(self.key)) {
-            log::warn!("Drop error: {:?}", err)
+            log::warn!("Drop warning: {:?}", err)
         }
     }
 }
