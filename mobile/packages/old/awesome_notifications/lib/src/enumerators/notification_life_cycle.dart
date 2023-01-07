7b1463d9d28094e610067a98b73fa001cf44b07a
Thomas Braun <braunth@oregonstate.edu>
2023-01-07 09:04:58 -0500

    prepare for open source release

--- mobile/packages/old/awesome_notifications/lib/src/enumerators/notification_life_cycle.dart
+++
@@ -1,2 +0,0 @@
-/// Application life cycle at new notification change state
-enum NotificationLifeCycle { Foreground, Background, AppKilled }
--- mobile/packages/old/awesome_notifications/lib/src/utils/html_utils.dart
+++
@@ -1,9 +0,0 @@
-class HtmlUtils {
-  static String removeAllHtmlTags(String htmlText) {
-    if (htmlText == null) return null;
-    RegExp exp =
-        RegExp(r"<[a-zA-Z\/][^>]*>", multiLine: true, caseSensitive: false);
-
-    return htmlText.replaceAll(exp, '');
-  }
-}
