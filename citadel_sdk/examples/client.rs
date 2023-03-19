f7845e2e1c1ffef3dbb2e16e7bd788825553f736
Thomas Braun <braunth@oregonstate.edu>
2023-03-19 10:43:10 -0400

    get the basic networking up

--- citadel_sdk/examples/client.rs
+++ citadel_sdk/examples/client.rs
@@ -5,6 +5,10 @@ use std::sync::atomic::{AtomicBool, Ordering};
 async fn main() {
     citadel_logging::setup_log();
     let addr = get_env("CITADEL_SERVER_ADDR");
+    let stun0 = get_env("STUN_0_ADDR");
+    let stun1 = get_env("STUN_1_ADDR");
+    let stun2 = get_env("STUN_2_ADDR");
+
     let finished = &AtomicBool::new(false);
     let client = citadel_sdk::prefabs::client::single_connection
     ::SingleClientServerConnectionKernel::new_register_defaults("Dummy user", "dummyusername", "notsecurepassword", addr, |_connection, remote| async move {
@@ -15,6 +19,7 @@ async fn main() {
 
     let _ = NodeBuilder::default()
         .with_node_type(NodeType::Peer)
+        .with_stun_servers([stun0, stun1, stun2])
         .build(client)
         .unwrap()
         .await
--- citadel_sdk/examples/server.rs
+++ citadel_sdk/examples/server.rs
@@ -7,11 +7,15 @@ use std::str::FromStr;
 async fn main() {
     citadel_logging::setup_log();
     let addr = get_env("CITADEL_SERVER_ADDR");
+    let stun0 = get_env("STUN_0_ADDR");
+    let stun1 = get_env("STUN_1_ADDR");
+    let stun2 = get_env("STUN_2_ADDR");
     let server = EmptyKernel::default();
     let _ = NodeBuilder::default()
         .with_node_type(NodeType::Server(
             SocketAddr::from_str(addr.as_str()).unwrap(),
         ))
+        .with_stun_servers([stun0, stun1, stun2])
         .build(server)
         .unwrap()
         .await
