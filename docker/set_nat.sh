b0cc00c9d8202bcd77f71033fc640c4a8c23ddd7
Thomas Braun <braunth@oregonstate.edu>
2023-03-24 13:40:27 -0400

    improve iptables rules

--- docker/set_nat.sh
+++ docker/set_nat.sh
@@ -12,30 +12,27 @@ echo "Container IP: $CONTAINER_IP"
 # Set up simulated latency
 tc qdisc add dev eth0 root netem delay 100ms
 
-# Configure iptables rules based on NAT type
+# Enable IP forwarding
+# echo 1 > /proc/sys/net/ipv4/ip_forward
 
+# Flush existing rules
+iptables -F
+iptables -t nat -F
+
+# Configure iptables rules based on NAT type
 case $NAT_TYPE in
 
   none)
     # No NAT translation (use default rules)
     echo "No NAT translation for $CONTAINER_IP"
+    exit 1
   ;;
 
   full_cone)
     # Full-cone NAT (one-to-one mapping of IP and port)
 
-     # Enable IP forwarding
-     echo 1 > /proc/sys/net/ipv4/ip_forward
-
-     # Flush existing rules
-     iptables -F
-     iptables -t nat -F
-
-     # Add MASQUERADE rule for outgoing packets
-     iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-
-     # Add DNAT rule for incoming packets
-     iptables -t nat -A PREROUTING -i eth0 -j DNAT --to-destination $CONTAINER_IP
+    iptables -t nat -A POSTROUTING -o eth1 -j SNAT --to-source $CONTAINER_IP
+    iptables -t nat -A PREROUTING -i eth1 -j DNAT --to-destination 0.0.0.0
 
      echo "Full-cone NAT translation for $CONTAINER_IP"
    ;;
@@ -43,15 +40,11 @@ case $NAT_TYPE in
    symmetric)
      # Symmetric NAT (different mapping of IP and port for each destination)
 
-     # Enable IP forwarding
-     echo 1 > /proc/sys/net/ipv4/ip_forward
-
-     # Flush existing rules
-     iptables -F
-     iptables -t nat -F
-
-     # Add MASQUERADE rule for outgoing packets with random port selection
-     iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE --random
+      echo 1 >/proc/sys/net/ipv4/ip_forward
+      iptables --flush
+      iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE --random
+      iptables -A FORWARD -i eth1 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+      iptables -A FORWARD -i eth0 -o eth1 -j ACCEPT
 
       echo "Symmetric NAT translation for $CONTAINER_IP"
    ;;
@@ -59,18 +52,7 @@ case $NAT_TYPE in
    port_restricted)
       # Port-restricted NAT (same mapping of IP and port for each destination, but only allow incoming packets from same source port)
 
-      # Enable IP forwarding
-      echo 1 > /proc/sys/net/ipv4/ip_forward
-
-      # Flush existing rules
-      iptables -F
-      iptables -t nat -F
-
-      # Add MASQUERADE rule for outgoing packets
-      iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-
-      # Add DNAT rule for incoming packets with matching source port
-      iptables -t nat -A PREROUTING -i eth0 -m state --state ESTABLISHED,RELATED -j DNAT --to-destination $CONTAINER_IP
+      iptables -t nat -A POSTROUTING -o eth0 -j SNAT --to-source $CONTAINER_IP
 
       echo "Port-restricted NAT translation for $CONTAINER_IP"
    ;;
@@ -78,18 +60,10 @@ case $NAT_TYPE in
    address_restricted)
        # Address-restricted NAT (same mapping of IP and port for each destination, but only allow incoming packets from same source address)
 
-       # Enable IP forwarding
-       echo 1 > /proc/sys/net/ipv4/ip_forward
-
-       # Flush existing rules
-       iptables -F
-       iptables -t nat -F
-
-       # Add MASQUERADE rule for outgoing packets
-       iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-
-       # Add DNAT rule for incoming packets with matching source address
-       iptables -t nat -A PREROUTING -i eth0 -m state --state ESTABLISHED,RELATED -j DNAT --to-destination $CONTAINER_IP
+       iptables -t nat -A POSTROUTING -o eth1 -p udp -j SNAT --to-source $CONTAINER_IP
+       iptables -t nat -A PREROUTING -i eth1 -p udp -j DNAT --to-destination 0.0.0.0
+       iptables -A INPUT -i eth1 -p udp -m state --state ESTABLISHED,RELATED -j ACCEPT
+       iptables -A INPUT -i eth1 -p udp -m state --state NEW -j DROP
 
        echo "Address-restricted NAT translation for $CONTAINER_IP"
     ;;
